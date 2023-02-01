9dbccef0a284826801e2e1da2905420d88a7d523
arthur@aclee.fr <arthur@aclee.fr>
2023-02-01 18:22:25 +0100

    ca va mieux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
+import { getByTitle } from '@testing-library/react';
 import './App.css'; 
 
 
 function App() {
+  resetTo();
+  render();
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
+}
+
+function resetTo(){
+  document.body.innerHTML = "";
+} 
+
+function render(){
+  title();
+  generic();
+  let div = document.createElement("div");
+  div.innerHTML = "Hello World";
+  //giv this to the div classname = "title"
+  div.setAttribute("class", "title");
+  document.body.appendChild(div);
+}
+
+function title(){
+    let div = document.createElement("h1");
+    div.innerHTML = "Ma Todo List :";
+    document.body.appendChild(div);
+}
+
+function generic(){
+   let div = document.createElement("div");
+
+  //add a input
+  let input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("id", "input");
+  input.setAttribute("placeholder", "Add a new task");
+  div.appendChild(input);
+
+  //add a button
+  let button = document.createElement("button");
+  button.setAttribute("id", "button");
+  button.innerHTML = "Add";
+  div.appendChild(button);
+  document.body.appendChild(div);
 }
 
 export default App;
