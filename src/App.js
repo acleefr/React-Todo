77c191b6ec6ecbc5a2bf15d075721fdb54302521
arthur@aclee.fr <arthur@aclee.fr>
2023-02-03 16:47:59 +0100

    ca marche

--- src/App.js
+++ src/App.js
@@ -1,67 +1,67 @@
 import './App.css';
 
 function App() {
+  //reset console
+  console.clear();
   render();
-  add();
   return (
-    <div>
-    </div>
+    <><input type="text" placeholder="Add a todo" /><button>Add</button><div>
+    </div></>
   );
 }
 
-function resetTo(){
-  document.body.innerHTML = "";
+function render() {
+  resetPage();
+  displayHeader();
+  displayInput();
+}
+
+function resetPage() {
+  document.body.innerHTML = '';
 } 
 
-function render(){
-  resetTo();
-  title();
-  generic();
-  displayTodo();
+function displayHeader() {
+  const header = document.createElement('h1');
+  header.textContent = 'Todo App';
+  document.body.appendChild(header);
 }
 
-function title(){
-    let div = document.createElement("h1");
-    div.innerHTML = "Ma Todo List :";
-    document.body.appendChild(div);
+function displayInput() {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Add a todo');
+  input.setAttribute('id', 'todo-input');
+  document.body.appendChild(input);
+  // add a button
+  const button = document.createElement('button');
+  button.textContent = 'Add';
+  document.body.appendChild(button);
+  // add event listener
+  button.addEventListener('click', () => {
+    displayTodo();
+  });
 }
 
-function generic(){
-   let div = document.createElement("div");
+function displayTodo() {
+  //stock input value in a variable
+  const input = document.getElementById('todo-input');
+  const inputValue = input.value;
+  console.log(inputValue);
 
-  //add a input that will take the task
-  let input = document.createElement("input");
-  input.setAttribute("type", "text");
-  input.setAttribute("id", "task");
-  input.setAttribute("placeholder", "Add a new task");
-  div.appendChild(input);
+  const todo = document.createElement('div');
+  todo.textContent = inputValue;
+  document.body.appendChild(todo);
 
-  //add a button that trigger the add function
-  let button = document.createElement("button");
-  button.setAttribute("id", "button");
-  button.setAttribute("onclick", "add()");
-  button.innerHTML = "+";
-  div.appendChild(button);
-  document.body.appendChild(div);
+  //reset input text
+  input.value = '';
+
+  //diplay a button to delete the todo
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Done';
+  todo.appendChild(deleteButton);
+  deleteButton.addEventListener('click', () => {
+    todo.remove();
+  });
 }
-
-function displayTodo(){
-  //add the todo stocked in the local storage to the div
-  let div = document.createElement("div");
-  div.setAttribute("id", "todo");
-  document.body.appendChild(div);
-}
- 
-if (document.getElementById("button")) {
-  console.log("button exist");
-}
-
-function add(){
-  console.log("add");
-  let task = document.getElementById("task").value;
-  let div = document.createElement("div");
-  div.innerHTML = task;
-  document.getElementById("todo").appendChild(div);
-} 
 
 export default App;
